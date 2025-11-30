cd741274ef1b8956cfad79a3065621c209b0873e
PixelRican <rimercado02@gmail.com>
2025-11-30 08:35:05 -0500

    Set blur in material
    I found a way to get rid of the function call and have the blur effect apply when the pause menu is opened.

--- pause_menu.gd
+++ pause_menu.gd
@@ -7,17 +7,12 @@ func _input(event: InputEvent) -> void:
 		else:
 			_on_pause_pressed()
 
-func _set_blur(value: float) -> void:
-	$BlurFilter.material.set_shader_parameter('blur', value)
-
 func _on_pause_pressed() -> void:
 	get_tree().paused = true
-	_set_blur(0.8)
 	show()
 
 func _on_resume_pressed() -> void:
 	get_tree().paused = false
-	_set_blur(0.0)
 	hide()
 
 func _on_exit_pressed() -> void:
