91d9a53e520bf01f1345f6d08755a5b9ecfdfdbd
PixelRican <rimercado02@gmail.com>
2025-11-29 19:47:29 -0500

    Updated menus

--- main_menu.gd
+++ main_menu.gd
@@ -1,9 +1,5 @@
 extends Control
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$MainButtonContainer/StartButton.grab_focus()
-
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
 
--- pause_menu.gd
+++ pause_menu.gd
@@ -1,28 +1,20 @@
 extends Control
 
-func _ready():
-	$AnimationPlayer.play("RESET")
-
-func resume():
-	get_tree().paused = false
-	$AnimationPlayer.play_backwards("blur")
-
-func pause():
-	get_tree().paused = true
-	$AnimationPlayer.play("blur")
-
-func _on_resume_pressed() -> void:
-	resume()
-
-func _on_quit_to_menu_pressed() -> void:
-	resume()
-	get_tree().change_scene_to_file("res://main_menu.tscn")
-
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("esc") and not get_tree().paused:
-		pause()
-	elif Input.is_action_just_pressed("esc") and get_tree().paused:
-		resume()
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action('ui_cancel'):
+		if get_tree().paused:
+			_on_resume_pressed()
+		else:
+			_on_pause_pressed()
 
 func _on_pause_pressed() -> void:
-	pause()
+	get_tree().paused = true
+	$AnimationPlayer.play('blur')
+
+func _on_resume_pressed() -> void:
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards('blur')
+
+func _on_quit_to_menu_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file('res://main_menu.tscn')
